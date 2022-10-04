5d82a0417d958e915469b642192500ff518f6f50
far-galaxy <far.galaxy.klg@gmail.com>
2022-10-04 15:21:29 +0400

    added index and stuff sites

--- server.py
+++ server.py
@@ -1,4 +1,34 @@
 # -*- coding: utf-8 -*-
 from libraries.utils import *
+from flask import *
 
-print(loadJSON("config"))
+app = Flask(__name__)
+
+@app.route("/") 
+def index():
+	return send_file("site/index.html")
+
+@app.route('/media/<path:path>', methods=['GET'])
+def get_media(path):
+	try:
+		return send_file(f"site/media/{path}")
+	except FileNotFoundError:
+		abort(404)
+		
+@app.route("/stuff") 
+def stuff():
+	page = request.args.get('page')
+	if page == None:
+		return send_file("stuff/index.html")
+	else:
+		return send_file("stuff/video.html")
+
+@app.route('/files/<path:path>', methods=['GET'])
+def stuff_files(path):
+	try:
+		return send_file("stuff/files/"+path)
+	except FileNotFoundError:
+		abort(404)		
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
